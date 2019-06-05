09afefdc961c79eef15d2560578c332a7b91de0c
Brandon Nichols <brandonaronnichols@gamil.com>
2019-06-05 17:43:30 -0500

    Changed for loops to be advanced array loops with for each

--- js/index.js
+++ js/index.js
@@ -43,16 +43,20 @@ logo.setAttribute('src', siteContent["nav"]["img-src"]);
 
 let navLinks = document.querySelectorAll('a');
 
-for(let i = 0; i < 6; i++){
-  navLinks[i].textContent = siteContent["nav"][`nav-item-${i + 1}`];
-}
+// for(let i = 0; i < 6; i++){
+//   navLinks[i].textContent = siteContent["nav"][`nav-item-${i + 1}`];
+// }
+
+navLinks.forEach((link, index) =>{
+  link.textContent = siteContent["nav"][`nav-item-${index + 1}`];
+});
 
 let ctaTxt = document.querySelector('.cta-text');
 //ctaTxt.removeChild('h1');
 
 let h1Array = [];
 let anyText = siteContent["cta"]["h1"].split(" ");
-console.log(anyText);
+
 h1Array = anyText.map(word => {
   let text = document.createElement('h1');
   text.textContent = word;
@@ -80,21 +84,29 @@ let mainSection = document.querySelectorAll('.text-content');
 
 mainContentText.splice(4, 1);
 
-for(let i = 0; i < mainSection.length; i++){
-  mainSection[i].children[0].textContent = mainContentText.shift();
-  mainSection[i].children[1].textContent = mainContentText.shift();
-  //can make more dynamic using for loop with children
-}
+// for(let i = 0; i < mainSection.length; i++){
+//   for(let j = 0; j < mainSection[i].childElementCount; j++){
+//     mainSection[i].children[j].textContent = mainContentText.shift();
+//   }
+// }
+
+mainSection.forEach(elem =>{
+  let textContentChildren = Array.from(elem.children);
+  textContentChildren.forEach(child => child.textContent = mainContentText.shift());
+});
 
 let midImg = document.querySelector('.middle-img');
 midImg.src = siteContent["main-content"]["middle-img-src"];
 
 let contactValues = Object.values(siteContent["contact"]);
 let contactSection = document.querySelector(".contact");
+let contactChildren = Array.from(contactSection.children);
 
-for(let i = 0; i < contactSection.childElementCount; i++){
-  contactSection.children[i].textContent = contactValues[i];
-}
+// for(let i = 0; i < contactSection.childElementCount; i++){
+//   contactSection.children[i].textContent = contactValues[i];
+// }
+
+contactChildren.forEach((elem, index) => elem.textContent = contactValues[index]);
 
 let footer = document.querySelector('footer p');
 
